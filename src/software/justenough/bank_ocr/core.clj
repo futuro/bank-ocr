a86bd4a8f411b6776d7d30231b98927204c82cc2
Evan Niessen-Derry <eniessenderry@gmail.com>
2019-02-15 16:56:45 -0600

    Clarify comments
    I wanted to make it clearer what was going on, so I've clarified some of
    the comments in the file, and removed some unnecessary functions.

--- src/software/justenough/bank_ocr/core.clj
+++ src/software/justenough/bank_ocr/core.clj
@@ -2,8 +2,8 @@
   (:require [clojure.string :as str]))
 
 (def ocr->int
-  "This contains a map of the characters that make up every number to the
-  integer they represent."
+  "This contains a map of the sequence of characters that make up every OCR number
+  to the integer they represent."
   {[\space \_ \space \| \space \| \| \_ \|]                 0
    [\space \space \space \space \space \| \space \space \|] 1
    [\space \_ \space \space \_ \| \| \_ \space]             2
@@ -15,7 +15,6 @@
    [\space \_ \space \| \_ \| \| \_ \|]                     8
    [\space \_ \space \| \_ \| \space \_ \|]                 9})
 
-;; TODO think of a better name.
 (defn ocr-str->char-seqs
   "Given an entry of OCR number characters, return each numbers' characters in
   order.
@@ -23,20 +22,22 @@
   F.e.
    _
   | |
-  |_| becomes `(\\space \\_ \\space \\| \\space \\| \\| \\_ \\|)`"
+  |_| becomes `((\\space \\_ \\space \\| \\space \\| \\| \\_ \\|))`"
   [entry]
-  (loop [;; Convert the entry from one string per row into one seq per 3
-         ;; characters, ordered by number
-         numbers  (->> entry
+  (loop [numbers  (->> entry
+                       ;; An entry may have an empty line, which we don't want
+                       ;; to keep
                        (remove empty?)
+                       ;; Each OCR number has 3 characters per row; split them
+                       ;; into individual seqs
                        (map #(partition 3 %))
+                       ;; Each OCR number has 3 rows; order each row's seq one
+                       ;; after the other, top-to-bottom and left-to-right
                        (apply interleave))
-         num-text (take 3 numbers)
          acc      []]
     (if (not-empty numbers)
-      (recur (drop 3 numbers)
-             (take 3 (drop 3 numbers))
-             (conj acc (apply concat num-text)))
+      (recur (drop 3 numbers) ; Drop the OCR number we're processing from the collection
+             (conj acc (apply concat (take 3 numbers))))
       acc)))
 
 (defn entry->int-str
@@ -48,19 +49,14 @@
        (map ocr->int)
        (str/join)))
 
-(defn process-entries
-  [entries]
-  (reduce (fn [num-strs entry]
-            (conj num-strs (entry->int-str entry)))
-          []
-          entries))
-
 (defn process-file
   "Given a file path to properly formatted input, return the integers
   represented by the OCR characters."
   [path]
   (->> path
        (slurp)
+       ;; XXX This will drop trailing newlines, so the last entry will be 3
+       ;; lines instead of 4
        (str/split-lines)
-       (partition-all 4)
-       (process-entries)))
+       (partition-all 4) ; Each entry is 4 lines, except the last
+       (map entry->int-str)))
