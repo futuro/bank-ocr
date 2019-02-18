d44b66833486fcf1f5ccbac9c21231196f38d2f3
Evan Niessen-Derry <eniessenderry@gmail.com>
2019-02-18 14:50:56 -0600

    Move shared account seqs to `test-util` ns
    I want to reuse these in the user story 3 test namespace, so I'm moving
    them somewhere everyone can get to.

--- test/acceptance/software/justenough/bank_ocr/test_user_story_2.clj
+++ test/acceptance/software/justenough/bank_ocr/test_user_story_2.clj
@@ -3,38 +3,13 @@
   didn't, strictly speaking, have acceptance criteria, I can think of some that
   may be worth exercising."
   (:require [clojure.test :as t :refer [deftest]]
-            [software.justenough.bank-ocr.core :as ocr]))
-
-(def valid-acc-nums
-  '((2 8 1 0 0 0 8 1 3)
-    (4 6 4 7 5 8 9 5 0)
-    (8 4 3 8 1 0 6 5 3)
-    (0 9 9 7 7 5 5 3 7)
-    (6 2 3 7 9 2 8 6 6)
-    (2 4 9 9 1 3 4 3 8)
-    (2 4 2 1 4 7 7 5 3)
-    (4 2 9 8 0 0 1 8 9)
-    (6 5 4 5 1 7 7 4 0)))
-
-(def invalid-acc-nums
-  '((8 9 9 1 7 7 6 6 6)
-    (5 2 7 1 2 0 8 2 8)
-    (2 8 5 2 0 5 8 0 3)
-    (2 1 6 1 4 8 1 7 7)
-    (4 0 3 7 3 5 4 6 4)
-    (6 0 4 6 8 0 1 8 0)
-    (6 9 9 3 5 4 2 1 3)
-    (2 4 1 7 6 7 1 9 9)
-    (3 0 8 4 5 5 2 8 4)
-    (8 7 7 0 8 6 1 2 2)
-    (2 8 2 8 8 4 2 8 6)
-    (8 2 6 3 2 3 0 8 8)
-    (3 5 2 3 9 3 4 8 1)))
+            [software.justenough.bank-ocr.core :as ocr]
+            [software.justenough.bank-ocr.test-util :as util]))
 
 (deftest valid-account-numbers
-  (doseq [acc-num valid-acc-nums]
+  (doseq [acc-num util/valid-acc-nums]
     (t/is (ocr/valid-entry? acc-num))))
 
 (deftest invalid-account-numbers
-  (doseq [acc-num invalid-acc-nums]
+  (doseq [acc-num util/invalid-acc-nums]
     (t/is (not (ocr/valid-entry? acc-num)))))
