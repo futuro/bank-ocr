c9d45e56531afb33355a5039be49edd3b7d22074
Evan Niessen-Derry <eniessenderry@gmail.com>
2019-02-18 14:52:46 -0600

    Add tests for User Story 3
    I've added three files of OCR entries to be used in this test, which
    match the account numbers in `util/valid-acc-nums`,
    `util/invalid-acc-nums`, and then another copy of `util/valid-acc-nums`
    with pipes and underscores removed at semi-random to make illegible
    entries.

---
+++ test/acceptance/software/justenough/bank_ocr/test_user_story_3.clj
@@ -0,0 +1,20 @@
+(ns software.justenough.bank-ocr.test-user-story-3
+  (:require [clojure.test :as t :refer [deftest]]
+            [software.justenough.bank-ocr.core :as ocr]
+            [clojure.string :as str]
+            [software.justenough.bank-ocr.test-util :as util]))
+
+(deftest valid-account-numbers
+  (let [processed-entries (ocr/process-file "valid-entries.txt")]
+    (t/is (= (map str/join util/valid-acc-nums)
+             processed-entries))))
+
+(deftest invalid-account-numbers
+  (let [processed-entries (ocr/process-file "invalid-entries.txt")]
+    (t/is (every? #(str/ends-with? % " ERR") processed-entries))
+    (t/is (= (map #(str (str/join %) " ERR") util/invalid-acc-nums)
+             processed-entries))))
+
+(deftest illegible-account-numbers
+  (let [processed-entries (ocr/process-file "illegible-entries.txt")]
+    (t/is (every? #(str/ends-with? % " ILL") processed-entries))))
